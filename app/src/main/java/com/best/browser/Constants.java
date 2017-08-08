d3fa772192dda5810f6c5aac95db61c7e6fb1f40
tongchengtianqi <1124564003@qq.com>
2017-08-08 12:31:35 +0800

    广告通广告

---
+++ app/src/main/java/com/best/browser/Constants.java
@@ -0,0 +1,8 @@
+package com.best.browser;;
+
+public class Constants {
+  public static final String APPID = "1104874641";
+  public static final String BannerPosID = "8060122437128253";
+  public static final String InterteristalPosID = "4050626457420261";
+  public static final String SplashPosID = "9070827437723290";
+}
